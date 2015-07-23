ddf4fca6a0dae8d62039aedfd86ecd2ab2ec6ac4
Matthias Dittrich <Matthias.Dittrich@aitgmbh.de>
2015-07-23 16:22:57 +0200

    remove header from GlobalAssemblyInfo

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,16 +1,4 @@
-﻿// ---------------------------------------------------------------------------------------------------------------
-// Copyright (C) 2011 AIT - Applied Information Technologies GmbH & Co. KG All Rights Reserved
-// AIT - Applied Information Technologies Confidential Proprietary Information
-// This Software is confidential and proprietary to AIT - Applied Information Technologies.
-// The reproduction or disclosure in whole or in part to anyone outside of AIT - Applied Information Technologies
-// without the written approval of an officer of AIT - Applied Information Technologies GmbH & Co. KG,
-// under a Non-Disclosure Agreement, or to any employee who has not previously
-// obtained a written authorization for access from the individual responsible
-// for the software will have a significant detrimental effect on
-// AIT - Applied Information Technologies and is expressly PROHIBITED.
-// ---------------------------------------------------------------------------------------------------------------
-
-#region Using Directives
+﻿#region Using Directives
 
 using System.Reflection;
 using System.Resources;
